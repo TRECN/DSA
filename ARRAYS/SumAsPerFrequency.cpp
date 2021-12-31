d3343bb4b81c2a5518630e6217031204003fc2a1
TRECN <rishabh1103@outlook.com>
2021-12-31 22:46:08 +0530

    commit

--- ARRAYS/SumAsPerFrequency.cpp
+++ ARRAYS/SumAsPerFrequency.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    n>>q;
+    int n,q,l,r;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }   
+    cin>>q;
     while(q--){
         cin>>l;
         cin>>r;
