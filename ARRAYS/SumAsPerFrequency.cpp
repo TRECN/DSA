234d7ecd9e9ef003ac08bba4515c96a8937f4454
TRECN <rishabh1103@outlook.com>
2022-01-02 22:45:44 +0530

    commit

--- ARRAYS/SumAsPerFrequency.cpp
+++ ARRAYS/SumAsPerFrequency.cpp
@@ -10,29 +10,7 @@ int main(){
         a1.push_back(input);
     }   
     cin>>q;
-    while(q--){
-        vector<int> a(a1);
-        
-        cin>>l;
-        cin>>r;
-        for(int i=0;i<n;i++){
-            int temp=a[i];
-            if(temp){
-                int j=0,f=0,s=0;
-                while(j<n){
-                    if(temp==a[j]){
-                        f++;
-                        s+=a[j];
-                        a[j]=0;
-                    } 
-                    j++;
-                }
-                if(f>=l&&f<=r){
-                    sum+=s;
-                }
-                
-            }
-        }
+    
         cout<<sum<<endl; 
         sum=0;    
     }   
