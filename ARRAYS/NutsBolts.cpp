f182c0a18edbe41eb319120b5817ebbb64bb76f0
TRECN <rishabh1103@outlook.com>
2022-01-03 22:44:04 +0530

    commit

--- ARRAYS/NutsBolts.cpp
+++ ARRAYS/NutsBolts.cpp
@@ -3,7 +3,8 @@ using namespace std;
 class Solution{
     public:
         void matchPairs(char nuts[],char bolts[],int n){
-            sort()
+            sort(nuts,nuts+n);
+            sort(bolts,bolts+n);
         }
 };
 int main(){
