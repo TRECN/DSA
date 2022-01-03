908ac3eb0915582d63a61b5e5e288b90f65bc869
TRECN <rishabh1103@outlook.com>
2022-01-03 22:51:47 +0530

    commit

--- ARRAYS/kLargest.cpp
+++ ARRAYS/kLargest.cpp
@@ -1,5 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        auto ans = ob.kLargest(arr,n,k);
+        for(auto x:ans){
+            cout<<x<<" ";
+
+        }
+        cout<<endl;
+    }
 }
