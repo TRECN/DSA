00af02a6bbbb32dd181d773a5c462443decee1fd
TRECN <rishabh1103@outlook.com>
2022-01-03 22:55:23 +0530

    commit

--- ARRAYS/kLargest.cpp
+++ ARRAYS/kLargest.cpp
@@ -5,7 +5,12 @@ class Solution{
         vector<int> kLargest(int arr[],int n,int k){
             sort(arr,arr+n);
             vector<int> a(k);
-            
+            for(int i=n-1,j=0;i>0;i++,j++){
+                if(k--){
+                    break;
+                }
+                a[j]=arr[i];
+            }
         }
 };
 int main(){
