dedef2e06a9c377152643703281e035665fce5ad
TRECN <rishabh1103@outlook.com>
2022-01-03 21:59:43 +0530

    commit

--- ARRAYS/countTheZeros.cpp
+++ ARRAYS/countTheZeros.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution{
     public:
         int countZeroes(int arr[],int n){
-            int c;
+            int c=0;
             for(int i=0;i<n;i++){
                 if(arr[i]==0){
                     c++;
