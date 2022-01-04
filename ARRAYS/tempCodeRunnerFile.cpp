2fd32883e9f6e91059dd54c6f9a25f12f46f9240
TRECN <rishabh1103@outlook.com>
2022-01-05 02:08:18 +0530

    commit

--- ARRAYS/tempCodeRunnerFile.cpp
+++ ARRAYS/tempCodeRunnerFile.cpp
@@ -1,1 +1,12 @@
-0
+            void reverse(){
+    struct node* a=start;
+    struct node* b=NULL;
+    struct node* c;
+    while(a!=NULL){
+        c=a->next;
+        a->next=b;
+        b=a;
+        a=c;
+    }
+    start=b;
+}
