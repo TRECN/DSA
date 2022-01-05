65da46d4595b2ea44d66345ab7bd65e192263e99
TRECN <rishabh1103@outlook.com>
2022-01-05 23:07:39 +0530

    commit

--- ARRAYS/IdenticalLinkedLists.cpp
+++ ARRAYS/IdenticalLinkedLists.cpp
@@ -47,7 +47,12 @@ bool areIdentical(struct Node* head1,struct Node *head2){
     Node* temp1 = head1;
     Node* temp2 = head2; 
     while(true){
-        
+        if((temp1==NULL&&temp2!=NULL)||(temp1!=NULL&&temp2==NULL))
+            return 0;
+        if(temp1==NULL&&temp2==NULL)
+            return 1;
+        if(temp1->data!=temp2->data)
+            return 0;
 
         temp1=temp1->next;
         temp2=temp2->next;
