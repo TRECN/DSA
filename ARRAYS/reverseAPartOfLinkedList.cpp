d3f7f7a09af84ccc026de4d4aa5a72304d74c7bd
TRECN <rishabh1103@outlook.com>
2022-01-07 22:10:06 +0530

    commit

--- ARRAYS/reverseAPartOfLinkedList.cpp
+++ ARRAYS/reverseAPartOfLinkedList.cpp
@@ -1,14 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct Node{
-    int data;
-    Node* next;
-    Node(int x){
-        data=x;
-        next=NULL;
-    }
-};
 
 
 void printList(struct Node* head){
