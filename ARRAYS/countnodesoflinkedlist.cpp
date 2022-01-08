1a725e4805e30880c184097ba27565a529be0826
TRECN <rishabh1103@outlook.com>
2022-01-08 23:14:13 +0530

    commit

--- ARRAYS/countnodesoflinkedlist.cpp
+++ ARRAYS/countnodesoflinkedlist.cpp
@@ -30,6 +30,15 @@ int main(){
         int n;
         cin>>n;
 
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for(int i=0;i<n-1;i++){
+            cin>>data;
+            tail->next=new Node(data);
+            tail=tail->next;
+        }
         Solution ob;
         cout<<ob.getCount(head)<<endl;
     }
