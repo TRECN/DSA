569171d0f9741e4ad107c2c74c6a6d3b1dd9252e
TRECN <rishabh1103@outlook.com>
2022-01-08 23:17:17 +0530

    commit

--- ARRAYS/RotateBy90.cpp
+++ ARRAYS/RotateBy90.cpp
@@ -1,4 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+void swap(int* a,int* b){
+    int temp=*b;
+    *b=*a;
+    *a=temp;
+}
+void rotate(vector<vector<int>>& matrix){
+    int n=matrix.size();
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            swap(&matrix[i][j],&matrix[j][i]);
+        }
+    }
+    for(int i=0;i<n/2;i++){
+        for(int j=0;j<n;j++){
+            swap(&matrix[i][j],&matrix[n-1-i][j]);
+        }
+    }
 
+}
 int main(){
     int t;
     cin>>t;
