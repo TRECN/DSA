f346266b9425485294d701214fb5b2a7271f493b
TRECN <rishabh1103@outlook.com>
2022-01-08 23:11:09 +0530

    commit

--- ARRAYS/countnodesoflinkedlist.cpp
+++ ARRAYS/countnodesoflinkedlist.cpp
@@ -11,17 +11,6 @@ struct Node{
     }
 };
 
-class Solution{
-    public:
-        int getCount(struct Node* head){
-            int count=0;
-            while(head!=NULL){
-                ++count;
-                head=head->next;
-            }
-            return count;
-        }
-};
 
 int main(){
     int t;
