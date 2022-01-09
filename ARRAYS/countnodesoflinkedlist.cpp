8949d4d429f219c9ce22de25cecf9b07e14f5d1f
TRECN <rishabh1103@outlook.com>
2022-01-09 21:35:27 +0530

    commit

--- ARRAYS/countnodesoflinkedlist.cpp
+++ ARRAYS/countnodesoflinkedlist.cpp
@@ -1,15 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-struct Node{
-    int data;
-    struct Node* next;
-
-    Node(int x){
-        data = x;
-        next = NULL;
-    }
-};
 
 class Solution{
     public:
