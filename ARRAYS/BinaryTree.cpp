42741498a37d14f4553204286ccc3e21e0aef450
TRECN <rishabh1103@outlook.com>
2022-01-09 21:33:33 +0530

    commit

--- ARRAYS/BinaryTree.cpp
+++ ARRAYS/BinaryTree.cpp
@@ -27,22 +27,7 @@ Node* InsertNode(Node* root, int data){
     q.push(root);
 
     while(!q.empty()){
-        Node* temp = q.front();
-        q.pop();
-
-        if(temp->left!=NULL)
-            q.push(temp->left);
-        else{
-            temp->left = CreateNode(data);
-            return root;
-        }
-
-        if(temp->right !=NULL)
-            q.push(temp->right);
-        else{
-            temp->right =  CreateNode(data);
-            return root;
-        }
+       
     }
 }
 
