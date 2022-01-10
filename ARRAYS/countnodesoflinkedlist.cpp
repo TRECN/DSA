6560d426565b231248cde85b9c39cacb714a2d2f
TRECN <rishabh1103@outlook.com>
2022-01-10 23:03:57 +0530

    commit

--- ARRAYS/countnodesoflinkedlist.cpp
+++ ARRAYS/countnodesoflinkedlist.cpp
@@ -23,23 +23,3 @@ class Solution{
         }
 };
 
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-
-        int data;
-        cin>>data;
-        struct Node *head = new Node(data);
-        struct Node *tail = head;
-        for(int i=0;i<n-1;i++){
-            cin>>data;
-            tail->next=new Node(data);
-            tail=tail->next;
-        }
-        Solution ob;
-        cout<<ob.getCount(head)<<endl;
-    }
-}
