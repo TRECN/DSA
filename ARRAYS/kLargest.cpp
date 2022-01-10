0a08674240ea867b15c749cd1c4fa9a1362462b7
TRECN <rishabh1103@outlook.com>
2022-01-10 23:02:57 +0530

    commit

--- ARRAYS/kLargest.cpp
+++ ARRAYS/kLargest.cpp
@@ -1,20 +1,4 @@
-#include<bits/stdc++.h>
-using namespace std;
-class Solution{
-    public:
-        vector<int> kLargest(int arr[],int n,int k){
-            sort(arr,arr+n);
-            vector<int> a;
-            int j=1;
-            while(k--){
-                a.push_back(arr[n-j]);
-                j++;
 
-            }
-            return a;
-        }
-        
-};
 int main(){
     int t;
     cin>>t;
