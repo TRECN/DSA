18eefb8d9262132ad18f3d8538d788415ff97a95
TRECN <rishabh1103@outlook.com>
2022-01-10 23:02:40 +0530

    commit

--- ARRAYS/NutsBolts.cpp
+++ ARRAYS/NutsBolts.cpp
@@ -20,15 +20,7 @@ int main(){
         for(int i=0;i<n;i++){
             cin>>bolts[i];
         }
-        Solution ob;
-        ob.matchPairs(nuts,bolts,n);
-        for(int i=0;i<n;i++){
-            cout<<nuts[i]<<" ";
-        }
-        cout<<endl;
-        for(int i=0;i<n;i++){
-            cout<<bolts[i]<<" ";
-        }
+  
         cout<<endl;
     }
 }
