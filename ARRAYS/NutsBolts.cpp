8d95d659408862720e1d3d2c373330f6b57c3b3e
TRECN <rishabh1103@outlook.com>
2022-01-11 22:22:50 +0530

    commit

--- ARRAYS/NutsBolts.cpp
+++ ARRAYS/NutsBolts.cpp
@@ -11,15 +11,7 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        int n;
-        cin>>n;
-        char nuts[n],bolts[n];
-        for(int i=0;i<n;i++){
-            cin>>nuts[i];
-        }
-        for(int i=0;i<n;i++){
-            cin>>bolts[i];
-        }
+
         Solution ob;
         ob.matchPairs(nuts,bolts,n);
         for(int i=0;i<n;i++){
