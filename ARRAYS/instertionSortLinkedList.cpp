8fd0bd97e047e148eec1945f57d29a09171afbf2
TRECN <rishabh1103@outlook.com>
2022-01-11 22:20:27 +0530

    commit

--- ARRAYS/instertionSortLinkedList.cpp
+++ ARRAYS/instertionSortLinkedList.cpp
@@ -42,7 +42,12 @@ class Solution{
 int main(){
     int t;
     cin>>t;
-  
+    while(t--){
+        int n;
+        cin>>n;
+        Node *head = NULL;
+        Node *temp = head;
+        
         for(int i=0;i<n;i++){
             int data;
             cin>>data;
