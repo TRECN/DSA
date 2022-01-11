39f3fdfb89dfeba3b21efd7aa9e173329e49c826
TRECN <rishabh1103@outlook.com>
2022-01-11 22:19:18 +0530

    commit

--- ARRAYS/instertionSortLinkedList.cpp
+++ ARRAYS/instertionSortLinkedList.cpp
@@ -1,4 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+struct Node{
+    int data;
+    struct Node *next;
+    Node(int x){
+        data=x;
+        next=NULL;
+    }
+};
 
 void printList(struct Node *head){
     struct Node *temp=head;
