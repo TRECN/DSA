fd5031c8bd0f34662f0281db21d2969287c94bf8
TRECN <rishabh1103@outlook.com>
2022-01-11 22:24:12 +0530

    commit

--- ARRAYS/RotateBy90.cpp
+++ ARRAYS/RotateBy90.cpp
@@ -25,7 +25,19 @@ int main(){
     while(t--){
         int n;
         cin>>n;
-        
+        vector<vector<int>>matrix(n);
+        for(int i=0;i<n;i++){
+            matrix[i].resize(n);
+            for(int j=0;j<n;j++){
+                cin>>matrix[i][j];
+            }
+        }
+        rotate(matrix);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++)
+                cout<<matrix[i][j]<<" ";
+            cout<<endl;
+        }
 
     }
     return 0;
