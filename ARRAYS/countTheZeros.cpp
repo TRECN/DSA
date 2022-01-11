34dd0de6dcf6c9b54768b902580f42ab8ffb7182
TRECN <rishabh1103@outlook.com>
2022-01-11 22:24:43 +0530

    commit

--- ARRAYS/countTheZeros.cpp
+++ ARRAYS/countTheZeros.cpp
@@ -14,18 +14,3 @@ class Solution{
         }
 };
 
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        int arr[n];
-        for(int i=0;i<n;i++){
-            cin>>arr[i];
-        }
-        Solution ob;
-        auto ans = ob.countZeroes(arr,n);
-        cout<<ans<<endl;
-    }
-}
