72c7b71c2a224a163f49862b88c76aee6226a2a9
TRECN <rishabh1103@outlook.com>
2022-01-12 21:39:42 +0530

    commit

--- ARRAYS/IdenticalLinkedLists.cpp
+++ ARRAYS/IdenticalLinkedLists.cpp
@@ -16,7 +16,18 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-      
+        int n1,n2, tmp, d1, d2;
+        struct Node* head1 = NULL, *tail1=NULL;
+        struct Node* head2 = NULL, *tail2=NULL;
+        cin>>n1;
+        cin>>d1;
+        head1 = new Node(d1);
+        tail1 = head1; 
+        while(n1-->1){
+            cin>>tmp;
+            tail1->next=new Node(tmp);
+            tail1 = tail1->next;
+        }
         cin>>n2;
         cin>>d2;
         head2 = new Node(d2);
