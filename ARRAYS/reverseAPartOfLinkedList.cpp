7557cd0cbd11cbbac1adcb1e602147c24569b65c
TRECN <rishabh1103@outlook.com>
2022-01-12 21:37:34 +0530

    commir

--- ARRAYS/reverseAPartOfLinkedList.cpp
+++ ARRAYS/reverseAPartOfLinkedList.cpp
@@ -80,5 +80,26 @@ class Solution{
 int main(){
     int t;
     cin>>t;
+    while(t--){
+        int N,m,n;
+        cin>>N>>m>>n;
+        Node *head=NULL;
+        Node *temp=head;
 
+        for(int i=0;i<N;i++){
+            int data;
+            cin>>data;
+            if(head==NULL){
+                head=temp=new Node(data);
+            }
+            else{
+                temp->next=new Node(data);
+                temp=temp->next;
+            }
+        }
+        Solution ob;
+        Node* newhead=ob.reverseBetween(head,m,n);
+        printList(newhead);
+        cout<<"\n";
+    }
 }
