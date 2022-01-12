86468abba99a519c5aa6a546029ae43be5592740
TRECN <rishabh1103@outlook.com>
2022-01-12 21:38:01 +0530

    commit

--- ARRAYS/reverseAPartOfLinkedList.cpp
+++ ARRAYS/reverseAPartOfLinkedList.cpp
@@ -11,13 +11,7 @@ struct Node{
 };
 
 
-void printList(struct Node* head){
-    struct Node* temp=head;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
-        temp=temp->next;
-    }
-}
+
 
 void reverse(Node* M){
             Node* a=M;
