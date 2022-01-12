bdbb3840d87687f07e280ae08d204cb069dec91c
TRECN <rishabh1103@outlook.com>
2022-01-12 21:39:22 +0530

    commit

--- ARRAYS/IdenticalLinkedLists.cpp
+++ ARRAYS/IdenticalLinkedLists.cpp
@@ -1,14 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct Node{
-    int data;
-    struct Node* next;
-    Node(int x){
-        data = x;
-        next = NULL;
-    }
-};
+
 
 bool areIdentical(struct Node* a,struct Node *b);
 
